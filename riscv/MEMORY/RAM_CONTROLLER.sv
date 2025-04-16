4d6a04d4057564542a9c214b53a7d1e9f84148ed
Alexandr Shipin <ssanek212@gmail.com>
2025-04-16 15:05:24 +0300

    FIXED RAM CONTROLLER

--- riscv/MEMORY/RAM_CONTROLLER.sv
+++ riscv/MEMORY/RAM_CONTROLLER.sv
@@ -22,7 +22,7 @@ module RAM_CONTROLLER #(
     input wire read_trigger,
     output wire[DATA_SIZE-1: 0] read_value,
     output wire read_value_ready,
-
+    output logic[2:0] led0,
 
     // MIG INTERFACE
 
@@ -35,7 +35,7 @@ module RAM_CONTROLLER #(
     // Сигналы для записи (подаются в Write Data FIFO MIG)
     output logic [CHUNK_PART - 1:0]          mig_app_wdf_data, // 128-битные данные для записи
     output logic                  mig_app_wdf_end,  // Сигнал завершения передачи burst данных
-    output logic [(CHUNK_PART / 8 - 1):0]           mig_app_wdf_mask, // Маска для 128-битных данных (если нужна)
+    output wire [(CHUNK_PART / 8 - 1):0]           mig_app_wdf_mask, // Маска для 128-битных данных (если нужна)
     output logic                  mig_app_wdf_wren, // Разрешение записи в Write Data FIFO
     input  wire                  mig_app_wdf_rdy, // MIG готов принять данные для записи
 
@@ -62,10 +62,14 @@ module RAM_CONTROLLER #(
     typedef enum logic [1:0] {
         RAM_CONTROLLER_STATE_WATING,
         RAM_CONTROLLER_STATE_READ,
-        RAM_CONTROLLER_STATE_WRITE
+        RAM_CONTROLLER_STATE_WRITE,
+        RAM_CONTROLLER_STATE_INIT
     } RAM_CONTROLLER_STATE; 
 
     SYNC_CONTROLLER_STATE controll_clk_state;
+    SYNC_CONTROLLER_STATE _controll_clk_state;
+    
+    SYNC_CONTROLLER_STATE _controll_ui_clk_state;
     SYNC_CONTROLLER_STATE controll_ui_clk_state;
 
     logic[ADDRESS_SIZE - 1:0] internal_address;
@@ -87,8 +91,9 @@ module RAM_CONTROLLER #(
     assign read_value = internal_output_value[31:0];
 
     initial begin
-        controll_ui_clk_state = SYNC_CONTROLLER_NOT_CONTOLL;
-        controll_clk_state = SYNC_CONTROLLER_ACTIVE_CONTROLL;
+        controll_ui_clk_state = SYNC_CONTROLLER_ACTIVE_CONTROLL;
+        controll_clk_state = SYNC_CONTROLLER_NOT_CONTOLL;
+        
         internal_address = 0;
         internal_mask = 0;
         controller_ready = 1;
@@ -100,8 +105,13 @@ module RAM_CONTROLLER #(
         internal_read_value_ready2 = 0;
         internal_read_value_ready = 0;
     end
+    
 
     always_ff @(posedge clk) begin
+        led0[1] <= mig_init_calib_complete;
+        led0[0] <= !mig_init_calib_complete;
+        led0[2] <= !controll_clk_state == SYNC_CONTROLLER_ACTIVE_CONTROLL;
+        _controll_ui_clk_state <= controll_ui_clk_state;
         if (controll_clk_state == SYNC_CONTROLLER_ACTIVE_CONTROLL) begin
             controller_ready <= !(write_trigger || read_trigger) && (internal_error == 0);
             
@@ -122,41 +132,43 @@ module RAM_CONTROLLER #(
             end
         end else if (
             controll_clk_state == SYNC_CONTROLLER_WILL_STOP_CONTROLL && 
-            controll_ui_clk_state == SYNC_CONTROLLER_WILL_START_CONTROLL
+            _controll_ui_clk_state == SYNC_CONTROLLER_WILL_START_CONTROLL
         ) begin
             controll_clk_state <= SYNC_CONTROLLER_NOT_CONTOLL;
         end else if (
-            controll_ui_clk_state == SYNC_CONTROLLER_WILL_STOP_CONTROLL &&
+            _controll_ui_clk_state == SYNC_CONTROLLER_WILL_STOP_CONTROLL &&
             controll_clk_state == SYNC_CONTROLLER_NOT_CONTOLL
         ) begin
             controll_clk_state <= SYNC_CONTROLLER_WILL_START_CONTROLL;
         end else if (
             controll_clk_state == SYNC_CONTROLLER_WILL_START_CONTROLL &&
-            controll_ui_clk_state == SYNC_CONTROLLER_NOT_CONTOLL
+            _controll_ui_clk_state == SYNC_CONTROLLER_NOT_CONTOLL
         ) begin 
             controll_clk_state <= SYNC_CONTROLLER_ACTIVE_CONTROLL;
         end
     end
 
     initial begin 
-        ram_state = RAM_CONTROLLER_STATE_WATING;
+        ram_state = RAM_CONTROLLER_STATE_INIT;
         internal_mask = ~0;
         internal_chunk_part_index = 0;
     end
 
     // эти сигналы постоянны
-    assign mig_app_wdf_wren = 1;
-    assign mig_app_wdf_data = internal_write_value;
-    assign mig_app_wdf_end = 1;
     assign mig_app_addr = internal_address;
-    assign mig_app_wdf_mask = 1;
+    assign mig_app_wdf_mask = 16'b0000000000000000;
  
     always_ff @(posedge mig_ui_clk) begin
-        // тут код не требующий синхронизации
+        _controll_clk_state <= controll_clk_state;
 
         // код синхронизации
         if (controll_ui_clk_state == SYNC_CONTROLLER_ACTIVE_CONTROLL) begin
-            if (ram_state == RAM_CONTROLLER_STATE_WATING) begin 
+            if (ram_state == RAM_CONTROLLER_STATE_INIT) begin
+                if (mig_init_calib_complete) begin
+                    ram_state <= RAM_CONTROLLER_STATE_WATING;
+                    controll_ui_clk_state <= SYNC_CONTROLLER_WILL_STOP_CONTROLL;
+                end
+            end else if (ram_state == RAM_CONTROLLER_STATE_WATING) begin 
                 if (mig_app_rdy && mig_init_calib_complete) begin
 
                     if (internal_read_trigger) begin
@@ -166,12 +178,10 @@ module RAM_CONTROLLER #(
                         ram_state <= RAM_CONTROLLER_STATE_READ;
                     end else if (internal_write_trigger) begin
                         mig_app_en <= 1;
-                        mig_app_cmd <= 2;
+                        mig_app_cmd <= 3'b000;
 
-                        // mig_app_wdf_wren <= 1;
-                        // mig_app_wdf_data <= internal_write_value;
-                        // mig_app_wdf_end <= 1;
-
+                        mig_app_wdf_data[31:0] <= internal_write_value;
+                       
                         ram_state <= RAM_CONTROLLER_STATE_WRITE;
                     end else begin 
                         internal_error <= 1;
@@ -179,34 +189,43 @@ module RAM_CONTROLLER #(
                     end
                 end
             end else if (ram_state == RAM_CONTROLLER_STATE_READ) begin
-                mig_app_en <= 0;
+                if (mig_app_rdy) begin
+                    mig_app_en <= 0;
+                end
                 if (mig_app_rd_data_valid) begin
-                    internal_output_value <= mig_app_rd_data;
-                    // сохраняем сигнал и передаем управление
+                    internal_output_value <= mig_app_rd_data[31:0];
+
                     ram_state <= RAM_CONTROLLER_STATE_WATING;
                     controll_ui_clk_state <= SYNC_CONTROLLER_WILL_STOP_CONTROLL;
                 end
             end else if (ram_state == RAM_CONTROLLER_STATE_WRITE) begin
-                mig_app_en <= 0;
-                if (mig_app_wdf_rdy) begin
-                    // сигналы уже выставлены и синхронизированные так что просто передаем управление другому домену
+                if (mig_app_rdy) begin
+                    mig_app_en <= 0;
+                end
+                if (!mig_app_en && !mig_app_wdf_wren && mig_app_wdf_rdy) begin
+                    mig_app_wdf_wren <= 1;
+                    mig_app_wdf_end <= 1;
+                end else if (mig_app_wdf_wren) begin
+                    mig_app_wdf_wren <= 0;
+                    mig_app_wdf_end <= 0;
                     ram_state <= RAM_CONTROLLER_STATE_WATING;
                     controll_ui_clk_state <= SYNC_CONTROLLER_WILL_STOP_CONTROLL;
                 end
             end
         end else if (
             controll_ui_clk_state == SYNC_CONTROLLER_WILL_STOP_CONTROLL && 
-            controll_clk_state == SYNC_CONTROLLER_WILL_START_CONTROLL
+            _controll_clk_state == SYNC_CONTROLLER_WILL_START_CONTROLL
         ) begin
+            mig_app_en <= 0;
             controll_ui_clk_state <= SYNC_CONTROLLER_NOT_CONTOLL;
         end else if (
-            controll_clk_state == SYNC_CONTROLLER_WILL_STOP_CONTROLL &&
+            _controll_clk_state == SYNC_CONTROLLER_WILL_STOP_CONTROLL &&
             controll_ui_clk_state == SYNC_CONTROLLER_NOT_CONTOLL
         ) begin
             controll_ui_clk_state <= SYNC_CONTROLLER_WILL_START_CONTROLL;
         end else if (
             controll_ui_clk_state == SYNC_CONTROLLER_WILL_START_CONTROLL &&
-            controll_clk_state == SYNC_CONTROLLER_NOT_CONTOLL
+            _controll_clk_state == SYNC_CONTROLLER_NOT_CONTOLL
         ) begin 
             controll_ui_clk_state <= SYNC_CONTROLLER_ACTIVE_CONTROLL;
         end
